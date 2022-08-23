8a9a20f5abe04361e53470ae7a38a06dd8dc2918
郁凯 <yukai@jiagouyun.com>
2022-08-23 16:06:52 +0800

    WindiCss引入

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <el-row class="mb-4">
+  <!-- <el-row class="mb-4">
     <el-button>Default</el-button>
     <el-button type="primary">Primary</el-button>
     <el-button type="success">Success</el-button>
     <el-button type="info">Info</el-button>
     <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
-  </el-row>
+  </el-row> -->
+  <button class="btn">按钮</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+.btn {
+  @apply bg-purple-500 text-indigo-200 px-4 py-2 mx-4 my-4 transition-all duration-500 rounded hover:(bg-purple-900) focus:(ring-grey-4);
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
+import "virtual:windi.css";
+
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
